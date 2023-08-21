bcf2a173c4d0a510f303377db5f807b698c59efb
218022429 <ngwenyabrendeline@gmail.com>
2023-08-21 15:56:42 +0200

    1st commit

--- taxi.sql.js
+++ taxi.sql.js
@@ -8,20 +8,20 @@ const  db = await sqlite.open({
 
 await db.migrate();
 
-export async function joinQueue(passenger_queue_count) {
+export async function joinQueue() {
     // console.log('join queue')
-    const sql = `add into taxi_queue (passenger_queue_count) values (?,?,?)`
-    await db.run(sql, [passenger_queue_count]) 
+    const sql = `update taxi_queue set passenger_queue_count = passenger_queue_count + 1)`
+    await db.run(sql) 
 
 }
 
 export async function leaveQueue(passenger_queue_count) {
-    const sql = `delete into taxi_queue (passenger_queue_count) values (?,?,?)`
+    const sql = `update taxi_queue set passenger_queue_count = passenger_queue_count - 1`
     await db.run(sql, [passenger_queue_count]) 
 }
 
 export async function joinTaxiQueue(taxi_queue_count) {
-    const sql = `add into taxi_queue (taxi_queue_count) values (?,?,?)`
+    const sql = `update taxi_queue set taxi_queue_count = taxi_queue_count + 1`
     await db.run(sql, [taxi_queue_count])
 }
 
